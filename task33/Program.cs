63a7b4bb4cd9dece776a918e77ddcb5cdc8b2cf4
oleg <legik7127@gmail.com>
2022-07-09 19:29:39 +0300

    added task33

---
+++ task33/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует
+// ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Clear();
+int[] Ar = { 1, 5, 0, 7, -9 };
+int b=0;
+int NumberInarray(int[] Arr, int x)
+{
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        if (Arr[i] == x)
+        {
+            b = 1;
+            //Console.WriteLine("Да");
+            //Arr[i]==x? Console.WriteLine("Да"): Console.WriteLine("Нет");
+    
+        }
+    
+    }
+return (b);
+}
+NumberInarray(Ar, 7);
+if (b > 0)
+{
+  Console.WriteLine("Да");  
+}
+else Console.WriteLine("Нет");
