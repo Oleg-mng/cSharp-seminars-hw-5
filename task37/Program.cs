19537639a45f705c8cf605cdcb4fe883747fb615
oleg <legik7127@gmail.com>
2022-07-11 20:59:02 +0300

    added task37

---
+++ task37/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 37: 
+// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 12 10
+Console.Clear();
+int[] Ar = { 1, 2, 3, 4, 5};
+//           0  1  2  3  4
+int[] NewAr = new int[Ar.Length/2];
+void Newarraywithdualcomposition(int[] Arr)
+{
+    for (int i = 0; i < Ar.Length/2-1; i++)
+    {
+        NewAr[i]= Ar[i] * Ar[Ar.Length-i-1];
+    }
+        NewAr[(Ar.Length/2)]= Ar[(Ar.Length/2)];
+      
+}
+Newarraywithdualcomposition(Ar);
+System.Console.WriteLine(string.Join(", ", NewAr));
--- task38/Program.cs
+++ task38/Program.cs
@@ -1,36 +1,39 @@
-﻿// Задача 38: Задайте массив вещественных чисел. 
+﻿// C double не получается задать array через Random. Пробовал через "new Random().NextDouble"
+
+// Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 Console.Clear();
-int size = 5;
-int[] array = new int[size];
-
-void Fillarray(int[] Arr, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        Arr[i] = new Random().Next(-111, 111);
-    }
-}
-Fillarray(array, size);
+double size = 5;
+//double[] array = new double[size];
+double[] array = {-2, 1, 5.3, 7.6, -1.23};
+//void Fillarray(double[] Arr, double size)
+//{
+//     for (int i = 0; i < size; i++)
+//     {
+//         Arr[i] = new Random().Next(-111, 111);
+//     }
+//}
+//Fillarray(array, size);
 //System.Console.WriteLine($"Массив из вещественных чисел:");
 System.Console.WriteLine(string.Join(", ", array));
-void DeltaMaxMinOffarray(int[] Arr, int size)
+void DeltaMaxMinOffarray(double[] Arr, double size)
 {
-    int max = 0;
-    int min = 112;
+    double max = 0;
+    double min = 112;
     for (int i = 0; i < size; i++)
     {
         if (Arr[i] > max)
         {
             max = Arr[i];
+            //max = (Arr[i] > max) ? Arr[i] : 0;
         }
         if (Arr[i] < min)
         {
             min = Arr[i];
         }
     }
-    int m = max- min;
+    double m = max- min;
     System.Console.WriteLine($"максимальное значение : {max}");
     System.Console.WriteLine($"минимальное значение : {min}");
     System.Console.WriteLine($"разница  : {m}");
